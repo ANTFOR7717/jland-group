b26b7c2b32b89c0439b545b37bf35048c12d0a4a
ANTFOR7717 <anthony.m.forest@gmail.com>
2026-02-18 19:11:30 -0500

    feat: update hero components

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,10 +1,11 @@
 // @ts-check
 import { defineConfig } from 'astro/config';
-
+import editableRegions from '@cloudcannon/editable-regions/astro-integration';
 import tailwindcss from '@tailwindcss/vite';
 
 // https://astro.build/config
 export default defineConfig({
+  integrations: [editableRegions()],
   vite: {
     plugins: [tailwindcss()]
   }
